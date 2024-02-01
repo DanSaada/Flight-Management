ace56fd31a84bb9f523310129d5e3fd605fd905b
Dan Saada <dansaada2@gmail.com>
2024-02-01 15:47:57 +0200

    creating initial basic project structure

---
+++ flight-management-app/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Flight Management</title>
+      </Head>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      </main>
+    </>
+
+  );
+}
